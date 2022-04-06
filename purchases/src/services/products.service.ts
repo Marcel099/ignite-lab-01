c82e703f850ca44748846bd55e6cd9e2296bab54
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-04-06 13:13:15 -0300

    Config. rota de cadastro de produtos

--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -1,12 +1,43 @@
 import { Injectable } from "@nestjs/common";
+import slugify from "slugify";
 
 import { PrismaService } from "../database/prisma/prisma.service";
+
+interface CreateProductDTO {
+  title: string;
+
+} 
 
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
 
   listProducts() {
-    return this.prisma.product.findMany()
+    return this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: CreateProductDTO) {
+    const slug = slugify(title, {
+      lower: true,
+    });
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: {
+        slug,
+      }
+    })
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with same slug already exists.')
+    }
+
+    const product = await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      }
+    })
+
+    return product;
   }
 }

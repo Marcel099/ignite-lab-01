3dcbca06355915cc77424ef43dac7e7d382100e1
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-04-15 11:34:45 -0300

    Corr. do bug de carregamento da pg. de matrícula

--- web/src/lib/withPublicApollo.tsx
+++ web/src/lib/withPublicApollo.tsx
@@ -18,7 +18,7 @@ export function getApolloClient(
   ssrCache?: NormalizedCacheObject
 ) {
   const httpLink = createHttpLink({
-    uri: 'http://localhost:3333',
+    uri: 'http://localhost:3332/graphql',
     fetch,
   })
 
